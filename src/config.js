f944f097b9eff915a653a87446971bab21d41b5a
Jordan Mack <jordan.mack@brilliantnotion.com>
2022-01-08 15:24:08 -0800

    Increase transaction fee.

--- src/config.js
+++ src/config.js
@@ -21,6 +21,7 @@ const config =
 	tickMutex: new Mutex(), // A mutex to prevent two ticks from executing at the same time.
 	tickPostDelay: 1000, // Delay in milliseconds at the end of a tick before releasing the mutex.
 	transactionTimeoutDelay: 5 * 60 * 1000, // Delay in milliseconds before a confirming transaction will time out.
+	transactionFee: 100_000, // Amount in Shannons to be paid as the transaction fee.
 };
 
 export default config;
